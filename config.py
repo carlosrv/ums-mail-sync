968bb532a492041483f541548d8dae15180b9c30
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-10-22 16:22:53 -0400

    Add file config.

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+__author__ = 'josecarlos'
+
+#UMS
+ums_host = "mysql.example.edu.cu"
+ums_user = "user"
+ums_pass = "pass"
+ums_db = "db"
+
+#Economia
+eco_host = '10.37.34.247'
+eco_user = 'user'
+eco_pass = 'pass'
+eco_db = 'db'
+
+#Email
+email_host = "correo.example.edu.cu"
+email_user = "ums@example.edu.cu"
+email_pass = "123456"
+email_to = "red@example.edu.cu"
+email_subject = "Usuarios Bajas de UMS"
+email_body = 'El usuario %s fue dado de baja por Economia y de los Servicios de Red'
