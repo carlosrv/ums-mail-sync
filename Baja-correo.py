07fd162c4db01aadd85c48d06f9e6072dbbc260d
carlos <carlos.vergel@cnic.edu.cu>
2015-10-26 15:35:49 -0400

    changing service in ums

--- Baja-correo.py
+++ Baja-correo.py
@@ -4,6 +4,7 @@ import MySQLdb
 import pymssql
 import smtplib
 
+from config import *
 # User and password for mail
 
 def sendmail(solapin_baja):
@@ -56,16 +57,19 @@ def is_baja(solapin_ums):
 
 
 usuarios_a_dar_baja = []
-user_name = []
+usermail_baja = []
+userproxy_baja = []
+userjabber_baja = []
 
 for row_ums in cur_ums_accounts:
     if row_ums[10] and row_ums[1] == 1:
         solapin_ums = row_ums[10]
         name_ums = row_ums[5]
+        account_id = row_ums[0]
         # print 'solapin ums: ', solapin_ums
         if is_baja(solapin_ums) == 1:
             usuarios_a_dar_baja.append(solapin_ums.strip())
-            user_name.append(name_ums)
+
 
 # Cerrar la conexion con la db
 cur_ums_accounts.close()
@@ -81,11 +85,32 @@ def ums_baja(solapin_baja):
     sendmail(solapin_baja)
     return 1
 
+def mail_baja(id_baja, table, active, list, number_row):
+    cur_ums_mail = ums_accounts_conn.cursor()
+    cur_ums_mail.execute("SELECT * FROM %s WHERE account_id ='%s'" % (table, id_baja))
+    for mail_ums in cur_ums_mail:
+        if mail_ums:
+            usermail = mail_ums[number_row]
+            id_mail = mail_ums[0]
+            # print id_mail
+            if mail_ums[1] == 1:
+                cur_ums_mail.execute("UPDATE %s SET %s=0 WHERE id ='%s'" % (table, active, id_mail))
+                list.append([id_baja, usermail])
+    cur_ums_mail.close()
+    return 1
 
 for suspend_service in usuarios_a_dar_baja:
-    solapin_baja = suspend_service
+    solapin_baja = suspend_service[0]
+    id_baja = suspend_service[2]
     if ums_baja(solapin_baja) == 1:
         print suspend_service
+        mail_baja(id_baja, 'accounts_mailaccount', 'mail_active', usermail_baja, 5)
+        mail_baja(id_baja, 'accounts_jabberaccount', 'jabber_active', userjabber_baja, 4)
+        mail_baja(id_baja, 'accounts_proxyaccount', 'proxy_active', userproxy_baja, 4)
+
 
 
 print usuarios_a_dar_baja
+print usermail_baja
+print userproxy_baja
+print userjabber_baja
